2517da2e8cc9166df24d6ad1e1ca4199257a2bb3
kulklex <kulklex@gmail.com>
2023-04-03 18:38:45 +0100

    Impl Search Details Page

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -8,13 +8,23 @@ type Props = {}
 export default function SearchBar({}: Props) {
     const router = useRouter()
 
-    const handleSubmit = () => {}
+    const [searchTerm, setSearchTerm] = useState<string>('')
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault()
+
+        if(searchTerm) {
+            router.push(`/search/${searchTerm}`)
+            
+            setSearchTerm('')
+        }
+    }
 
   return (
     <Paper component="form" onSubmit={handleSubmit} 
         className="rounded-full  border-[1px]  pl-[2] shadow-none" sx={{mr: {sm:5}}}
     >
-        <input type="text" placeholder="Search..." onChange={() => {}}
+        <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search..."
             className="searchBar placeholder:p-1 p-1 border-[0] outline-none w-[200px] md:w-[300px] lg:w-[350px] bg-transparent" 
         />
         <IconButton type="submit" className='p-[10px] text-red-500'>
--- pages/search/[searchTerm].tsx
+++ pages/search/[searchTerm].tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { Typography, Box } from '@mui/material'
+import Videos from '../../components/Videos'
+import {fetchData} from '../../utils/fetchData'
+import {useRouter} from 'next/router'
 
-type Props = {}
+type Props = {
+  searchTerm: string
+}
 
-export default function SearchFeed({}: Props) {
+export default function SearchFeed({searchTerm}: Props) {
+  const router = useRouter()
+
+
+  const [videos, setVideos] = useState([])
+
+
+  useEffect(() => {
+    fetchData(`search?part=snippet&q=${searchTerm}`)
+    .then((data) => setVideos(data.items))
+    .catch((err) => console.error(err))
+  }, [searchTerm])
+  
   return (
-    <div>SearchTerm</div>
+  <Box p={2} minHeight="95vh" className="bg-black">
+    <Typography variant="h4" fontWeight={900}  color="white" mb={3} ml={{ sm: "100px"}}>
+      Search Results for <span style={{ color: "#FC1503" }}>{searchTerm}</span> 
+    </Typography>
+    <Box display="flex">
+      <Box sx={{ mr: { sm: '100px' } }}/>
+      {<Videos videos={videos} />}
+    </Box>
+  </Box>
   )
 }
+
+export const getServerSideProps = async ({params: {searchTerm}} : {params:{searchTerm: string}}) => {
+
+  return {
+    props: {
+      searchTerm
+    }
+  }
+}
+
