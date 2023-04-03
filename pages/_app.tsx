8a0d2cf9c3cfaec5e1424c08fd147b3b35176027
kulklex <kulklex@gmail.com>
2023-04-03 18:37:26 +0100

    Refactoring...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import Navbar from '../components/Navbar'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -15,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />  
     </Head>
-    
+    <Navbar />
     <Component {...pageProps} />
 </>)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
 
-import {  Box } from '@mui/material'
+import { Box } from '@mui/material'
 import Home from '../components/Home'
 
 
 const Index = () => {
  return (<Box className="bg-[#000]">
-  <Navbar />
+
   <Home />
  </Box>) 
 }
