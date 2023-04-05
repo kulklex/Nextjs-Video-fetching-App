b9cff16ebacc90003d320cfd9c8ca5522882a114
kulklex <kulklex@gmail.com>
2023-04-05 17:15:31 +0100

    updating configs

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['i.ibb.co', 'i.ytimg.com'],
+    domains: ['*','*.example.com','*.example.co', 'i.ibb.co', 'i.ytimg.com', 'yt3.ggpht.com'],
   }
 }
 
