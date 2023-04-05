10a6a4affbb99e15d9f95e43784a5d5c3a3ef566
kulklex <kulklex@gmail.com>
2023-04-05 17:38:55 +0100

    debugging

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['*','*.example.com','*.example.co', 'i.ibb.co', 'i.ytimg.com', 'yt3.ggpht.com'],
+    domains: ['*.example.com', 'i.ibb.co', 'i.ytimg.com', 'yt3.ggpht.com'],
   }
 }
 
